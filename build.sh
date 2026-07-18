6c8035c6edc7d82abedcb95d155de097ea556f4a
mohammedettour <blossoming.pack@gmail.com>
2026-07-18 16:50:38 +0100

    Include vendor dir and remove PHP from build step

--- build.sh
+++ build.sh
@@ -8,13 +8,4 @@ npm install
 echo "Building frontend..."
 npm run build
 
-echo "Moving to backend..."
-cd ../backend
-
-echo "Installing composer..."
-curl -sS https://getcomposer.org/installer | php
-
-echo "Installing backend dependencies..."
-php composer.phar install --no-dev --optimize-autoloader
-
 echo "Build complete!"
